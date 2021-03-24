fea7412da3bc1f11b7a33871e436962f345f755b
Raphael Koszalka <ctl_rkoszalka@uolinc.com>
2021-03-24 06:06:42 -0300

    chore(data-bureau-person-data) : ci

--- src/main/java/org/koszalka/pii/bureau/presentation/controller/PersonDataController.java
+++ src/main/java/org/koszalka/pii/bureau/presentation/controller/PersonDataController.java
@@ -7,7 +7,6 @@ import org.koszalka.pii.bureau.presentation.dto.PersonDataDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.koszalka.data.bureau.presentation.api.PersonDataAPI;
